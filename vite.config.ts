836b2a9c0a263ccbf7daa9fc3741225c419c275d
M.N. Jeral Sandeeptha <jeral.sandeeptha1@gmail.com>
2025-09-21 20:50:08 +0530

    Fixed-13

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -11,7 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dts()],
   test: {
     projects: [
       {
@@ -39,5 +40,21 @@ export default defineConfig({
         }
       }
     ]
-  }
+  },
+  build: {
+    lib: {
+      entry: 'src/index.ts',
+      name: 'UrlShortnerUiLib',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 });
